cc553fef6c5dca1057d2d6aa7fa87382afafac6f
Igor Silva <igor719@gmail.com>
2024-02-26 19:19:05 -0300

    Delete user by id

--- src/main/java/com/japaimport/workshopmongo/services/UserService.java
+++ src/main/java/com/japaimport/workshopmongo/services/UserService.java
@@ -36,5 +36,11 @@ public class UserService {
 		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
 	}
 	
+	public void delete(String id)
+	{
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	
 }
